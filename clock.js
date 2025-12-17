356f35f22678e5b363d7fcb8acf1380b6536696a
rajmondal9735670610-oss <rajmondal9735670610@gmail.com>
2025-12-17 18:23:18 +0530

    Implement clock display functionality

--- clock.js
+++ clock.js
@@ -1,1 +1,24 @@
 
+let hr = document.getElementById('hour');
+let min = document.getElementById('min');
+let sec = document.getElementById('sec');
+
+function displayTime(){
+    let date = new Date();
+
+    // Getting hour, mins, secs from date
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    let hRotation = 30*hh + mm/2;
+    let mRotation = 6*mm;
+    let sRotation = 6*ss;
+
+    hr.style.transform = rotate($(hRotationdeg));
+    min.style.transform = rotate($(mRotationdeg));
+    sec.style.transform = rotate($(sRotationdeg));
+
+}
+
+setInterval(displayTime, 1000);
